31805e4ef5e3fd4a3c5d971c355750182f7d5c95
Prabhu Dassan <prabhu.dc@sposea.com>
2025-05-21 13:49:47 +0000

    Added service to create a formula by parsing an AST tree

--- db/data-model.cds
+++ db/data-model.cds
@@ -5,20 +5,41 @@ entity Formulae : cuid, managed {
     description : String(255);
     formula : LargeString;
     dataRetrievalProxyObject : String(255);
-    Graph_root : Composition of one Nodes on Graph_root.formula = $self;
+    Nodes : Composition of many Nodes on Nodes.formula = $self;
+    Edges : Composition of many Edges on Edges.formula = $self;
     Models : Composition of many FormulaModels on Models.parent = $self;
+}
+
+// Graph setup
+// Vertices of the Graph
+entity Nodes  : cuid {
+    node_is_root : Boolean default false;
+    node_is_leaf : Boolean default false;
+    node_is_constant : Boolean default false;
+    node_is_variable : Boolean default false;
+    node_operator : String(5) default '';
+    node_operand : String(255) default '';
+    node_formula : String(255) default '';
+    formula : Association to Formulae;
+}
+
+// Edges of the Graph
+entity Edges : cuid {
+    start : Association to Nodes not null;
+    end : Association to Nodes not null;
+    edge_location : String(1) @assert.range: ['l', 'n', 'r'];
+    formula : Association to Formulae;
 }
 
 entity FormulaModels  {
     key parent : Association to Formulae;
-    key modelID : UUID; 
-    modelAlias : Association to TargetModels;
+    key model : Association to TargetModels; 
 }
 
 entity TargetModels: cuid, managed {
     targetModel : LargeString;
     schemaName : String(255);
-    modelAlias : String(255);
+    modelAlias : String(255);// Alias understandable to the user
 }
 
 entity ModelRelationships : cuid, managed {
@@ -32,24 +53,6 @@ entity ModelRelationshipAttributes : cuid, managed {
     relationship : Association to ModelRelationships;
     leftAttribute : String(255);
     rightAttribute : String(255);
+    // modelAlias : Association to TargetModels;
 }
 
-// Graph setup
-// Vertices of the Graph
-entity Nodes  : cuid {
-    node_is_root : Boolean;
-    node_is_leaf : Boolean;
-    node_is_constant : Boolean;
-    node_is_variable : Boolean;
-    node_operator : String(5);
-    node_operand : String(255);
-    node_formula : String(255);
-    formula : Association to Formulae;
-}
-
-// Edges of the Graph
-entity Edges : cuid {
-    start : Association to Nodes not null;
-    end : Association to Nodes not null;
-    edge_location : String(1) @assert.range: ['l', 'n', 'r'];
-}
--- srv/lib/core-services.js
+++ srv/lib/core-services.js
@@ -1,5 +1,6 @@
 
 const cds = require("@sap/cds");
+const math = require("mathjs");
 
 module.exports = {
   getFormulaById: async function (formulaID) {
@@ -27,7 +28,7 @@ module.exports = {
     /**
      * Retrieves the target HANA mocdel for the specified formula ID.
      * @param {string} formulaID - The ID of the formula to look up.
-     * @type {Promise<{ modelID: string } | undefined>}
+     * @type {Promise<{ model_ID: string } | undefined>}
      * @description Executes a SELECT query on the "FormulaModels" table to fetch the "modelID" where the "parent" matches the provided formulaID.
     */
    
@@ -35,22 +36,22 @@ module.exports = {
     try {
       const targetModelId = await SELECT.one
         .from("FormulaModels")
-        .columns("modelID")
+        .columns("model")
         .where({ parent: formulaID });
 
-      if (!targetModelId || !targetModelId.modelID)
+      if (!targetModelId || !targetModelId.model_ID)
         throw new Error(`Target model ID not found for formula ID ${formulaID}`);
 
       resultTargetModel = await SELECT.one
         .from("TargetModels")
         .columns("schemaName", "targetModel")
-        .where({ ID: targetModelId.modelID });
+        .where({ ID: targetModelId.model_ID });
       
       if (
         !resultTargetModel ||
         !resultTargetModel.schemaName ||
         !resultTargetModel.targetModel
-      )throw new Error(`Target model details not found for Target Model ID ${targetModelId.modelID}`);
+      )throw new Error(`Target model details not found for Target Model ID ${targetModelId.model_ID}`);
 
     } catch (error) {
       throw error;
@@ -125,4 +126,151 @@ module.exports = {
 
     return createDataRetrievalProxyObject; 
   },
+
+  
+
+  createVerticesAndEdgesFromAst : async function (ast, req) {
+      /**
+       * Parses the Abstract Syntax Tree (AST) and creates vertices and edges for the graph representation.
+       * @param {Object} ast - The Abstract Syntax Tree (AST) of the formula.
+       * @param {Object} req - The request object containing the formula data.
+       * @description The function traverses the AST, creating nodes and edges based on the node types and their relationships.
+       */
+      const nodeFormula = req.data.Nodes[0].node_formula;
+      const formulaID = req.data.ID; 
+      const nodeArray = [];
+      const edgeArray = [];
+      // Initialize the root node ID
+      const rootNodeID = cds.utils.uuid();
+      // Go over the AST and assign UUIDs to each node
+      // will be used as ID for Nodes entity
+      ast.traverse(function(node,path,parent) {
+        if (node.type === 'OperatorNode' || node.type === 'ConstantNode' || node.type === 'SymbolNode') {
+          node.ID = cds.utils.uuid();
+        } else {
+          node.ID = parent ? parent.ID:rootNodeID;
+        }
+      });
+      
+      // Create the root node
+      nodeArray.push({
+              ID: rootNodeID,
+              node_is_root: true,
+              node_is_leaf: false,
+              node_is_constant: false,
+              node_is_variable: false,
+              node_operator: '',
+              node_operand: '',
+              node_formula: nodeFormula,
+              formula_ID: formulaID
+            });
+    
+      const parentNodeIDSet = new Set();// To track left-hand side parent already visited
+      let edgeLocation = '';
+
+      // Helper to determine the edge location
+      // 'n' = first edge, 'l' = lhs, 'r' = rhs
+      ast.traverse(function (node, path, parent) {
+        if (edgeArray.length === 0) {
+          edgeLocation = 'n';
+        } else if (parent && parentNodeIDSet.has(parent.ID)) {
+          edgeLocation = 'r';
+        } else if (parent && !parentNodeIDSet.has(parent.ID)) {
+          parentNodeIDSet.add(parent.ID);
+          edgeLocation = 'l';
+        }
+
+        switch (node.type) {
+          case 'OperatorNode':
+            nodeArray.push({
+              ID: node.ID,
+              node_is_root: false,
+              node_is_leaf: false,
+              node_is_constant: false,
+              node_is_variable: false,
+              node_operator: node.op,
+              node_operand: '',
+              node_formula: '',
+              formula_ID: formulaID
+            });
+
+            edgeArray.push({
+              ID: cds.utils.uuid(),
+              start_ID: parent ? parent.ID : rootNodeID,
+              end_ID: node.ID,
+              edge_location: edgeLocation,
+              formula_ID: formulaID
+            });
+
+            break
+          case 'ConstantNode':
+            nodeArray.push({
+              ID: node.ID,
+              node_is_root: false,
+              node_is_leaf: true,
+              node_is_constant: true,
+              node_is_variable: false,
+              node_operator: '',
+              node_operand: node.value,
+              node_formula: '',
+              formula_ID: formulaID
+            });
+
+            edgeArray.push({
+              ID: cds.utils.uuid(),
+              start_ID: parent.ID,
+              end_ID: node.ID,
+              edge_location: edgeLocation,
+              formula_ID: formulaID
+            });
+
+            break
+          case 'SymbolNode':
+            nodeArray.push({
+              ID: node.ID,
+              node_is_root: false,
+              node_is_leaf: true,
+              node_is_constant: false,
+              node_is_variable: false,
+              node_operator: '',
+              node_operand: node.name,
+              node_formula: '',
+              formula_ID: formulaID
+            });
+            edgeArray.push({
+              ID: cds.utils.uuid(),
+              start_ID: parent.ID,
+              end_ID: node.ID,
+              edge_location: edgeLocation,
+              formula_ID: formulaID
+            });
+
+            break
+          default:
+            cds.log().info("Skipped Node",node.type)
+        }
+      });
+      return {"nodeArray": nodeArray, "edgeArray": edgeArray};
+    },
+
+  createFormulaEntryPayload: async function (req) {
+    /**
+     * Creates a payload for a new formula entry.
+     * @param {Object} req - The request object containing the formula data.
+     * @returns {Object} - The payload object for the new formula entry.
+     * @description The function extracts the formula data from the request and constructs a payload object
+     *              with the necessary properties for creating a new formula entry in the database.
+     */
+    
+    const formula = req.data.formula;
+    const nodeFormula = req.data.Nodes[0].node_formula;
+
+    // Parse the formula into an AST
+    const ast = math.parse(formula);
+    resultAstGraph = await this.createVerticesAndEdgesFromAst(ast, req);
+
+    req.data.Nodes = resultAstGraph.nodeArray;
+    req.data.Edges = resultAstGraph.edgeArray;
+    return ;
+  }
 };
