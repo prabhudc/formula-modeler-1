ff8a3e589a6b634d4ebed8592fc9d851f6776c65
Prabhu Dassan <prabhu.dc@sposea.com>
2025-07-08 18:43:26 +0000

    Added the details page and made it non-editable

--- app/editor/webapp/controller/FMMaintenanceView.controller.js
+++ app/editor/webapp/controller/FMMaintenanceView.controller.js
@@ -1,18 +1,22 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
+    "sap/m/Button",
     "sap/m/ColumnListItem",
     "sap/m/Text"
-], function(Controller, ColumnListItem, Text) {
+], function(Controller, Button, ColumnListItem, Text) {
     "use strict";
+
     return Controller.extend("fm.editor.controller.FMMaintenanceView", {
         onInit: function () {
             var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
             oRouter.getRoute("maintenance").attachPatternMatched(this._onRouteMatched, this);
         },
+
         _onRouteMatched: function(oEvent) {
             var formulaId = oEvent.getParameter("arguments").formulaId;
             var oTable = this.byId("targetModelsSelectedTable");
             var oView = this.getView();
+            var oModel = this.getView().getModel();
 
             // Bind FormulaModels table for this formulaId
             oTable.bindItems({
@@ -21,7 +25,7 @@ sap.ui.define([
                     $expand: "model",
                     $filter: "parent_ID eq '" + formulaId + "'"
                 },
-                template: new ColumnListItem({
+                template: new sap.m.ColumnListItem({
                     cells: [
                         new Text({ text: "{model/targetModel}" }),
                         new Text({ text: "{model/schemaName}" }),
@@ -36,7 +40,63 @@ sap.ui.define([
             if (oHeaderVBox) {
                 oHeaderVBox.bindElement({ path: sFormulaPath });
             }
+
+            // Query Formulae entity to retrieve the formula field and update the code editor
+            var oFormulaBinding = oModel.bindContext(sFormulaPath);
+            oFormulaBinding.requestObject().then(function(oData) {
+                var sFormulaContent = oData.formula;
+                var oCodeEditor = oView.byId("formulaCodeEditorMaintenanceView");
+                if (oCodeEditor) {
+                    oCodeEditor.setValue(sFormulaContent || ""); // Set formula content or empty string if undefined
+                }
+            }).catch(function(oError) {
+                console.error("Error retrieving formula content:", oError);
+            });
+
+            // Step 1: Query Nodes entity to retrieve the ID of the record where node_is_root is true
+            var sNodesPath = "/Nodes";
+            var oBinding = oModel.bindList(sNodesPath, undefined, undefined, [
+                new sap.ui.model.Filter("formula_ID", sap.ui.model.FilterOperator.EQ, formulaId),
+                new sap.ui.model.Filter("node_is_root", sap.ui.model.FilterOperator.EQ, true)
+            ]);
+
+            oBinding.requestContexts().then(function(aContexts) {
+                if (aContexts.length === 1) {
+                    var sNodeId = aContexts[0].getProperty("ID");
+
+                    // Step 2: Query Parameters entity using the retrieved node ID
+                    var sParametersPath = "/Parameters";
+                    var oParamBinding = oModel.bindList(sParametersPath, undefined, undefined, [
+                        new sap.ui.model.Filter("node_ID", sap.ui.model.FilterOperator.EQ, sNodeId),
+                        new sap.ui.model.Filter("parameter_type", sap.ui.model.FilterOperator.EQ, "formula_dimension"),
+                        new sap.ui.model.Filter("parameter_name", sap.ui.model.FilterOperator.EQ, "key")
+                    ]);
+
+                    oParamBinding.requestContexts().then(function(aParamContexts) {
+                        var aParameterValues = aParamContexts.map(function(oContext) {
+                            return oContext.getProperty("parameter_value");
+                        });
+
+                        // Step 3: Add buttons to the HBox with the retrieved parameter values
+                        var oHBox = oView.byId("keyFieldsButtonsContainerMaintenanceView");
+                        oHBox.removeAllItems(); // Clear existing buttons
+
+                        aParameterValues.forEach(function(sValue) {
+                            var oButton = new sap.m.Button({
+                                text: sValue,
+                                emphasized: true
+                            });
+                            oHBox.addItem(oButton);
+                        });
+                    });
+                } else {
+                    console.error("No root node found or multiple root nodes exist for formulaId:", formulaId);
+                }
+            }).catch(function(oError) {
+                console.error("Error retrieving nodes:", oError);
+            });
         },
+
         onReturnToLanding: function() {
             var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
             oRouter.navTo("landing");
--- srv/formula-modeler-services.cds
+++ srv/formula-modeler-services.cds
@@ -3,7 +3,9 @@ using FormulaModels as _formulamodels from '../db/data-model.cds';
 using TargetModels as _targetmodels from '../db/data-model.cds';
 using CVD_HANA_MODELS as _newModels  from '../db/data-model.cds';
 using CVD_MODEL_FIELDS as _modelFields from '../db/data-model.cds';
-using { cuid } from '@sap/cds/common';
+using Nodes as _nodes from '../db/data-model.cds';
+using Parameters as _parameters from '../db/data-model.cds';
+
 
 
 service FormulaModelerServices {
@@ -18,7 +20,11 @@ service FormulaModelerServices {
     entity NewModels as projection on _newModels;
     // Service to know the fields of the models
     entity ModelFields as projection on _modelFields;
-
+    // Nodes that for the vertices of the graph
+    entity Nodes as projection on _nodes;
+    // Parameters attached to each node
+    entity Parameters as projection on _parameters;
+    
     action buildFormulaOnModelbyFormulaID(formulaID : UUID,
                                  params : {
         keys    : array of String;
